dbaa84b3d2b470b7a124c5d651a6da1eb4772cac
6ivkin <fedorterentev48@gmail.com>
2023-06-01 16:49:29 +0400

    Product details page & cart

--- client/src/components/Qty.js
+++ client/src/components/Qty.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React from 'react'
+// context
 
-const Qty = () => {
-  return <div>Qty</div>;
+const Qty = ({ item }) => {
+  return (
+    <div>
+      {item.amount > 10 ? (
+        <select value={item.amount} className='p-2 rounded-lg w-[100px] h-12 outline-none text-primary'>select</select>
+      ) : (
+        <input
+         className='text-primary placeholder:text-primary h-12 rounded-md p-4 w-[120px] outline-accent'
+         type='text'
+         placeholder={`${item.amount}`}/>
+      )}
+    </div>
+  )
 };
 
 export default Qty;
